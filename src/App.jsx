501c7d933816d83626629548b1aca371e54e7e82
Anwar Khan <devscryptcomp@gmail.com>
2024-07-15 00:40:07 +0530

    Firebase initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import { useState } from "react";
-
+import { db } from "./utils/config";
+import { collection, addDoc } from "firebase/firestore";
 function App() {
+  const test = async () => {
+    console.time("test");
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    console.timeEnd("test");
+  };
   return (
     <>
-      <button className="bg-green-400 p-5 rounded-lg hover:bg-green-900 hover:text-white duration-500 transition-all">
+      <button
+        onClick={test}
+        className="p-5 transition-all duration-500 bg-green-400 rounded-lg hover:bg-green-300 "
+      >
         Hello World
       </button>
     </>
