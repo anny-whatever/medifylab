6cc74af733a5d1ebf6cf769799232ecad3ebfc95
Anwar Khan <devscryptcomp@gmail.com>
2024-07-14 19:45:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <button className="bg-green-400 p-5 rounded-lg hover:bg-green-900 hover:text-white duration-500 transition-all">
+        Hello World
+      </button>
+    </>
+  );
+}
+
+export default App;
