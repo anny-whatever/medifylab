975ff3e42b12e5573fd6c1fcb0cde2f6532576f5
Anwar Khan <devscryptcomp@gmail.com>
2024-07-21 06:15:17 +0530

    Update Bestseller.jsx

--- src/components/Bestseller.jsx
+++ src/components/Bestseller.jsx
@@ -5,7 +5,10 @@ function Bestseller() {
   return (
     <>
       {/* Card Blog */}
-      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <div
+        className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto"
+        id="bestseller"
+      >
         {/* Grid */}
         <div className="max-w-2xl mx-auto mb-10 text-center lg:mb-14">
           <h2 className="text-2xl font-bold md:text-4xl md:leading-tight text-primary">
