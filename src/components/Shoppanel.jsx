77dbef88b4599e15d338cc130673938c42c9ce20
Anwar Khan <devscryptcomp@gmail.com>
2024-07-25 13:18:58 +0530

    Shop page dynamic data and pagination

--- src/components/Shoppanel.jsx
+++ src/components/Shoppanel.jsx
@@ -1,10 +1,47 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { RadioGroup, Radio } from "@nextui-org/react";
 import { Pagination } from "@nextui-org/react";
 import { Accordion, AccordionItem } from "@nextui-org/react";
+import { useContext } from "react";
+import { DataContext } from "../utils/dataContext.js";
+import { Link } from "react-router-dom";
 
 import Shopcard from "./Shopcard";
 function Shoppanel() {
+  const { products } = useContext(DataContext);
+  const [category, setCategory] = useState("all");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentProducts, setCurrentProducts] = useState([]);
+
+  useEffect(() => {
+    let productsArray = [];
+    products?.productsArray?.map((product) => {
+      if (product.category === category || category === "all") {
+        productsArray.push(product);
+      }
+    });
+    setFilteredProducts(productsArray);
+  }, [category, products]);
+
+  const productsPerPage = 3;
+  const totalPages = Math.ceil(filteredProducts?.length / productsPerPage);
+
+  useEffect(() => {
+    // Get the products for the current page
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const current = filteredProducts?.slice(
+      indexOfFirstProduct,
+      indexOfLastProduct
+    );
+    setCurrentProducts(current);
+  }, [currentPage, filteredProducts, products]);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
   return (
     <div className="flex flex-col justify-center w-full md:flex-col ">
       <div className="flex flex-col justify-center w-full gap-x-7 md:flex-row">
@@ -16,7 +53,7 @@ function Shoppanel() {
             <RadioGroup
               label="Select product catergory"
               defaultValue={"all"}
-              // onChange={(value) => console.log(value.target.value)}
+              onChange={(value) => setCategory(value.target.value)}
               color="secondary"
             >
               <Radio value="all">All</Radio>
@@ -59,23 +96,44 @@ function Shoppanel() {
             All Products
           </h1>
 
-          <Shopcard />
-          <Shopcard />
-          <Shopcard />
-          <Shopcard />
-          <Shopcard />
-          <Shopcard />
-          <Shopcard />
-          <Shopcard />
+          {currentProducts?.map((product) => {
+            if (product.category === category || category === "all") {
+              return (
+                <Link key={product.uuid} to={`/product/${product.uuid}`}>
+                  <Shopcard
+                    title={product.name}
+                    price={product.price}
+                    discountedPrice={product.discount}
+                    description={product.description}
+                    mainImage={product.mainImage}
+                  />
+                </Link>
+              );
+            }
+          })}
+
+          {/* <Shopcard
+          // key={product.uuid}
+          // title={product.name}
+          // price={product.price}
+          // discountedPrice={product.discount}
+          // description={product.description}
+          // mainImage={product.mainImage}
+          /> */}
         </div>
       </div>
-      <Pagination
-        total={3}
-        color="secondary"
-        showControls
-        initialPage={1}
-        className="mx-auto my-5 "
-      />
+      {totalPages > 1 && (
+        <Pagination
+          total={totalPages}
+          color="secondary"
+          showControls
+          initialPage={1}
+          className="mx-auto my-5 "
+          onChange={(e) => {
+            handlePageChange(e);
+          }}
+        />
+      )}
     </div>
   );
 }
