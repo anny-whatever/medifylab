d4d11111c306058b451861616894a50cd915bdcf
Anwar Khan <devscryptcomp@gmail.com>
2024-07-28 13:24:37 +0530

    Cart is now functional

--- src/components/Cartcard.jsx
+++ src/components/Cartcard.jsx
@@ -1,69 +1,233 @@
 import React from "react";
+import { useContext, useEffect, useState } from "react";
 import { Button } from "@nextui-org/react";
 
-function Cartpanel() {
+import { db } from "../utils/config";
+import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+
+import { DataContext } from "../utils/dataContext";
+import { AuthContext } from "../utils/authContext";
+import { Toaster, toast } from "sonner";
+function Cartpanel({ cartpid, pid, qty, route, pack }) {
+  const { products } = useContext(DataContext);
+  const { userInfo, isLoggedIn, userData } = useContext(AuthContext);
+  const [productDetails, setProductDetails] = useState([]);
+  const [localQty, setLocalQty] = useState();
+
+  useEffect(() => {
+    const productDetails = products?.productsArray?.filter((product) => {
+      return product.uuid === pid;
+    });
+    setProductDetails(productDetails?.[0]);
+    console.log("sfihsdiuf");
+  }, [products]);
+
+  function incrementQtyLocal() {
+    let array = JSON.parse(localStorage.getItem("cart")) || [];
+
+    // Find the index of the object to update
+    const index = array.findIndex((obj) => obj.cartpid === cartpid);
+    console.log(index);
+    if (index !== -1) {
+      // Update the key in the found object
+      array[index]["qty"] = array[index]["qty"] + 1;
+
+      // Save the updated array back to local storage
+      setLocalQty(array[index]["qty"]);
+      localStorage.setItem("cart", JSON.stringify(array));
+      toast.success("Item Quantity Increased successfully");
+      window.location.reload();
+      return true; // Update successful
+    }
+
+    window.location.reload();
+  }
+
+  function decrementQtyLocal() {
+    let array = JSON.parse(localStorage.getItem("cart")) || [];
+
+    // Find the index of the object to update
+    const index = array.findIndex((obj) => obj.cartpid === cartpid);
+    console.log(index);
+    if (index !== -1) {
+      // Update the key in the found object
+      array[index]["qty"] = array[index]["qty"] - 1;
+
+      // Save the updated array back to local storage
+      setLocalQty(array[index]["qty"]);
+      localStorage.setItem("cart", JSON.stringify(array));
+      toast.success("Item Quantity Decreased successfully");
+      window.location.reload();
+      return true; // Update successful
+    }
+
+    window.location.reload();
+  }
+
+  function removeLocal() {
+    let array = JSON.parse(localStorage.getItem("cart")) || [];
+    const index = array.findIndex((obj) => obj.cartpid === cartpid);
+    let updatedCart = [...array.slice(0, index), ...array.slice(index + 1)];
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+    toast.success("Item Removed successfully");
+    window.location.reload();
+  }
+
+  const handleRemove = async () => {
+    await updateDoc(doc(db, "users", userInfo?.uid), {
+      cart: arrayRemove({ uuid: pid, qty: qty, route: route, pack: pack }),
+    }).catch((error) => {
+      console.log(error);
+    });
+    toast.success("Item removed successfully");
+  };
+
+  const handleIncrementQty = async () => {
+    updateDoc(doc(db, "users", userInfo?.uid), {
+      cart: arrayUnion({
+        cartpid: cartpid,
+        uuid: pid,
+        qty: qty + 1,
+        route: route,
+        pack: pack,
+      }),
+    });
+    updateDoc(doc(db, "users", userInfo?.uid), {
+      cart: arrayRemove({
+        cartpid: cartpid,
+        uuid: pid,
+        qty: qty,
+        route: route,
+        pack: pack,
+      }),
+    });
+    toast.success("Item Quantity Increased successfully");
+    window.location.reload();
+  };
+
+  const handleDecrementQty = async () => {
+    updateDoc(doc(db, "users", userInfo?.uid), {
+      cart: arrayUnion({
+        cartpid: cartpid,
+        uuid: pid,
+        qty: qty - 1,
+        route: route,
+        pack: pack,
+      }),
+    });
+    updateDoc(doc(db, "users", userInfo?.uid), {
+      cart: arrayRemove({
+        cartpid: cartpid,
+        uuid: pid,
+        qty: qty,
+        route: route,
+        pack: pack,
+      }),
+    });
+    toast.success("Item Quantity Decreased successfully");
+    window.location.reload();
+  };
+
+  const isBtnDisabled = () => {
+    if (localQty) {
+      return localQty <= 1 ? true : false;
+    } else {
+      return qty <= 1 ? true : false;
+    }
+  };
   return (
-    <div className="flex flex-col w-11/12 mx-auto mb-3 duration-200 ease-in-out md:flex-col hover:shadow-lg rounded-xl">
-      <div className="flex flex-col w-full overflow-hidden border-gray-200 sm:flex-row cartCard h-fit border-1 rounded-xl">
-        <div className="hidden w-full sm:block h-44 sm:max-w-52 sm:min-w-44 bg-slate-500 prodImg">
-          <img
-            src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg"
-            alt="abcd"
-            className="object-cover "
-          />
-        </div>
-        <div className="flex justify-between w-full p-3 bg-gradient-to-t from-[#ffffff] to-[#ffffff88] sm:bg-white">
-          <div className="flex flex-col justify-between ml-2 prodInfo">
-            <div className="div">
-              <div className="mt-3 text-xl title">Modafinin-100mg</div>
-              <div className="mt-2 quantity">Qty: 60 Pills</div>
-            </div>
-            <Button className="mb-3 delete w-fit" color="danger" variant="flat">
-              Remove
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                />
-              </svg>
-            </Button>
+    <>
+      <Toaster richColors />
+      <div className="flex flex-col w-11/12 mx-auto mb-3 duration-200 ease-in-out md:flex-col hover:shadow-lg rounded-xl">
+        <div className="flex flex-col w-full overflow-hidden border-gray-200 sm:flex-row cartCard h-fit border-1 rounded-xl">
+          <div className="hidden w-full sm:block min-h-44 sm:max-w-52 sm:min-w-44 bg-slate-500 prodImg">
+            <img
+              src={productDetails?.mainImage}
+              alt={productDetails?.name}
+              className="object-cover"
+            />
           </div>
-          <div className="flex flex-col items-end justify-between mt-3 mr-3 md:items-center jus prodInfo">
-            <div className="flex flex-col price">
-              <span className="mr-1 font-displaybold ">
-                <span className="mr-2 text-xl text-red-600 font-displaylight">
-                  -50%
-                </span>
-                <span className="text-2xl font-displaybold text-primary">
-                  $5
-                </span>
-                <span className="text-md text-primary">/pill</span>
-              </span>
-              <span className="font-displaylight">
-                MRP: <span className="line-through ">$10/pill</span>
-              </span>
+          <div className="flex justify-between w-full p-3 bg-gradient-to-t from-[#ffffff] to-[#ffffff88] sm:bg-white">
+            <div className="flex flex-col justify-between ml-2 prodInfo">
+              <div className="div">
+                <div className="mt-3 text-xl title">{productDetails?.name}</div>
+                <div className="mt-2 quantity">Pack size: {pack} Pills</div>
+                <div className="mt-2 quantity">
+                  Route: {route === "intous" ? "India to US" : "US to US"}
+                </div>
+              </div>
+              <Button
+                className="mb-3 delete w-fit"
+                color="danger"
+                variant="flat"
+                onClick={isLoggedIn ? handleRemove : removeLocal}
+              >
+                Remove
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </Button>
             </div>
-            <div className="flex flex-row items-center justify-center mb-3 sm:flex-col-reverse md:flex-row counter">
-              <Button isDisabled={true} size="sm" variant="flat" color="danger">
-                -
-              </Button>
-              <span className="mx-3">1</span>
-              <Button size="sm" variant="flat" color="secondary">
-                +
-              </Button>
+            <div className="flex flex-col items-end justify-between mt-3 mr-3 md:items-center jus prodInfo">
+              <div className="flex flex-col price">
+                <span className="mr-1 font-displaybold ">
+                  <span className="mr-2 text-xl text-red-600 font-displaylight">
+                    -
+                    {(
+                      ((productDetails?.price - productDetails?.discount) /
+                        productDetails?.price) *
+                      100
+                    ).toFixed(0)}
+                    %
+                  </span>
+                  <span className="text-2xl font-displaybold text-primary">
+                    ${productDetails?.discount}
+                  </span>
+                  <span className="text-md text-primary">/pill</span>
+                </span>
+                <span className="font-displaylight">
+                  MRP:{" "}
+                  <span className="line-through ">
+                    ${productDetails?.price}/pill
+                  </span>
+                </span>
+              </div>
+              <div className="flex flex-row items-center justify-center mb-3 sm:flex-col-reverse md:flex-row counter">
+                <Button
+                  isDisabled={isBtnDisabled()}
+                  size="sm"
+                  variant="flat"
+                  color="danger"
+                  onClick={isLoggedIn ? handleDecrementQty : decrementQtyLocal}
+                >
+                  -
+                </Button>
+                <span className="mx-3">{localQty ? localQty : qty}</span>
+                <Button
+                  size="sm"
+                  variant="flat"
+                  color="secondary"
+                  onClick={isLoggedIn ? handleIncrementQty : incrementQtyLocal}
+                >
+                  +
+                </Button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Cartpanel.jsx
+++ src/components/Cartpanel.jsx
@@ -1,49 +1,144 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@nextui-org/react";
 
 import Cartcard from "./Cartcard";
+import { DataContext } from "../utils/dataContext";
+import { AuthContext } from "../utils/authContext.js";
+import { useContext } from "react";
 function Cartpanel() {
+  const { userInfo, isLoggedIn, userData } = useContext(AuthContext);
+  const { products } = useContext(DataContext);
+  const [cart, setCart] = useState([]);
+  const [subtotal, setSubtotal] = useState(0);
+  const [shippingIndia, setShippingIndia] = useState(0);
+  const [shippingUs, setShippingUs] = useState(0);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      const productDetails = JSON.parse(localStorage.getItem("cart"));
+      setCart(productDetails);
+    }
+  }, [isLoggedIn]);
+
+  useEffect(() => {
+    function checkRoutes(array) {
+      let hasIntous = false;
+      let hasUstous = false;
+
+      if (array?.length > 0) {
+        for (const obj of array) {
+          if (obj.route === "intous") {
+            hasIntous = true;
+          } else if (obj.route === "ustous") {
+            hasUstous = true;
+          }
+
+          // setSubtotal(obj.qty * productDetails?.discount + Subtotal);
+        }
+      }
+
+      if (hasIntous) {
+        setShippingIndia(40);
+      }
+
+      if (hasUstous) {
+        setShippingUs(70);
+      }
+    }
+    if (!userData?.cart) {
+      checkRoutes(cart);
+    } else {
+      checkRoutes(userData?.cart);
+    }
+
+    function calculateTotal(array) {
+      const subtotal = array?.reduce((total, item) => {
+        const itemTotal = item.qty * item.price * item.pack;
+        return total + itemTotal;
+      }, 0);
+      console.log(subtotal);
+      setSubtotal(subtotal);
+    }
+
+    if (!userData?.cart) {
+      calculateTotal(cart);
+    } else {
+      calculateTotal(userData?.cart);
+    }
+  }, [cart]);
+  // console.log(userData);
   return (
     <>
       <div className="w-3/5 pl-3 mx-auto mt-5 mb-10 text-center lg:mb-6">
         <h2 className="text-3xl font-bold md:text-4xl md:leading-tight text-primary">
           Shopping cart
         </h2>
-        <p className="mt-2 text-sm text-gray-500">Your cart is empty</p>
+        {userData?.cart?.length === 0 ? (
+          <p className="mt-2 text-sm text-gray-500">Your cart is empty</p>
+        ) : (
+          <p className="mt-2 text-sm text-gray-500">
+            Your cart contains{" "}
+            {isLoggedIn ? userData?.cart?.length : cart?.length} item(s)
+          </p>
+        )}
+
         {/* <hr className="mt-3 border-gray-200" /> */}
       </div>
 
       <div className="flex flex-col justify-center w-full mb-10 lg:flex-row">
         <div className="w-full lg:w-3/5">
-          <Cartcard />
-          <Cartcard />
-          <Cartcard />
-          <Cartcard />
-          <Cartcard />
-          <Cartcard />
+          {isLoggedIn == true
+            ? userData?.cart
+                ?.sort((a, b) => a.pack - b.pack)
+                ?.map((item, index) => (
+                  <Cartcard
+                    cartpid={item.cartpid}
+                    key={index}
+                    pid={item.uuid}
+                    qty={item.qty}
+                    route={item.route}
+                    pack={item.pack}
+                  />
+                ))
+            : cart
+                ?.sort((a, b) => a.pack - b.pack)
+                ?.map((item, index) => (
+                  <Cartcard
+                    cartpid={item.cartpid}
+                    key={index}
+                    pid={item.uuid}
+                    qty={item.qty}
+                    route={item.route}
+                    pack={item.pack}
+                  />
+                ))}
         </div>
         <div className="flex flex-col w-11/12 p-10 mx-auto lg:mr-5 lg:mx-0 lg:w-2/5 bg-opacity-30 h-fit rounded-xl md:sticky top-10 bg-accent xl:w-1/4 gap-y-5">
           <p className="text-xl font-semibold">Order summary</p>
           <span className="flex justify-between">
             <p>Subtotal</p>
-            <p className="">$100</p>
+            <p className="">${subtotal}</p>
           </span>
           <hr />
-          <span className="flex justify-between">
-            <p>Shipping</p>
-            <p className="">$20</p>
+          <span className="flex justify-between text-sm">
+            <p>Shipping India to US</p>
+            <p className="">${shippingIndia}</p>
+          </span>
+          <span className="flex justify-between text-sm">
+            <p>Shipping US to US</p>
+            <p className="">${shippingUs}</p>
           </span>
           <hr />
-
-          <span className="flex justify-between">
-            <p>Tax</p>
-            <p className="">$5</p>
+          <span className="flex justify-between ">
+            <p>Total Shipping</p>
+            <p className="">${shippingIndia + shippingUs}</p>
           </span>
           <hr />
 
           <span className="flex justify-between text-xl font-semibold">
             <p>Total</p>
-            <p className="">$125</p>
+            <p className="">${subtotal + shippingIndia + shippingUs}</p>
           </span>
           <Button color="primary" className="w-full mt-5">
             Place order
