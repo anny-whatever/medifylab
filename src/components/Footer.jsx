9d9d5a4788d71cdbcc98361ec59c96ffda3e3574
Anwar Khan <devscryptcomp@gmail.com>
2024-07-28 13:24:52 +0530

    Footer sticks to bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,7 +26,7 @@ function Footer() {
     <>
       {/* ========== FOOTER ========== */}
       <Toaster richColors />
-      <footer className="w-full mt-auto bg-zinc-800">
+      <footer className="w-full mt-auto top-full bg-zinc-800">
         <div className="mt-auto w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 lg:pt-20 mx-auto">
           {/* Grid */}
           <div className="grid grid-cols-2 gap-6 md:grid-cols-4 lg:grid-cols-5">
