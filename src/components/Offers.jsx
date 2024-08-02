a1dd1f7204a2687516987439d463f488e2146914
Anwar Khan <devscryptcomp@gmail.com>
2024-08-02 15:45:54 +0530

    Update Offers.jsx

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -23,51 +23,62 @@ export default function Offers() {
         </div>
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {/* Icon Block */}
-          {products?.productsArray?.slice(0, 4).map((product) => (
-            <div
-              className="flex flex-col h-full p-5 transition-all duration-300 border border-gray-200 group hover:border-transparent hover:shadow-lg rounded-xl"
-              href="#"
-              key={product.uuid}
-            >
-              <div className="aspect-w-16 aspect-h-11">
-                <img
-                  className="object-cover w-full rounded-xl"
-                  src={product.mainImage}
-                  alt="Image Description"
-                />
+          {products?.productsArray
+            ?.sort((a, b) => a.discount - b.discount)
+            .slice(0, 4)
+            ?.map((product) => (
+              <div
+                className="relative flex flex-col p-5 transition-all duration-300 bg-white border border-gray-200 cursor-pointer group hover:border-transparent hover:shadow-lg rounded-xl h-96"
+                href="#"
+                key={product.uuid}
+              >
+                <div className="overflow-hidden h-1/2 rounded-xl">
+                  <img
+                    className="object-cover w-full rounded-xl "
+                    src={product.mainImage}
+                    alt="Image Description"
+                  />
+                </div>
+                <div className="flex flex-col justify-around w-full mt-1">
+                  <div className="my-2">
+                    <h3 className="text-xl font-semibold text-gray-600">
+                      {product.name}
+                    </h3>
+                    <p className="flex flex-col mt-1 text-gray-600 top-full">
+                      <span className="mr-1 font-displaybold ">
+                        <span className="mr-2 text-xl text-red-600 font-displaylight">
+                          -
+                          {(
+                            ((product.price - product.discount) /
+                              product.price) *
+                            100
+                          ).toFixed(0)}
+                          %
+                        </span>
+                        <span className="text-2xl font-displaybold text-primary">
+                          ${product.discount}
+                        </span>
+                        <span className="text-md text-primary">/pill</span>
+                      </span>
+                      <span className="font-displaylight">
+                        MRP:{" "}
+                        <span className="line-through ">
+                          ${product.price}/pill
+                        </span>
+                      </span>
+                    </p>
+                  </div>
+                  <Link to={`/product/${product.uuid}`}>
+                    <Button
+                      className="absolute w-[86.5%] bottom-5"
+                      color="secondary"
+                    >
+                      View
+                    </Button>
+                  </Link>
+                </div>
               </div>
-              <div className="my-2">
-                <h3 className="text-xl font-semibold text-gray-600">
-                  {product.name}
-                </h3>
-                <p className="flex flex-col mt-1 text-gray-600 top-full">
-                  <span className="mr-1 font-displaybold ">
-                    <span className="mr-2 text-xl text-red-600 font-displaylight">
-                      -
-                      {(
-                        ((product.price - product.discount) / product.price) *
-                        100
-                      ).toFixed(0)}
-                      %
-                    </span>
-                    <span className="text-2xl font-displaybold text-primary">
-                      ${product.discount}
-                    </span>
-                    <span className="text-md text-primary">/pill</span>
-                  </span>
-                  <span className="font-displaylight">
-                    MRP:{" "}
-                    <span className="line-through ">${product.price}/pill</span>
-                  </span>
-                </p>
-              </div>
-              <Link to={`/product/${product.uuid}`}>
-                <Button className="w-full view" color="secondary">
-                  View
-                </Button>
-              </Link>
-            </div>
-          ))}
+            ))}
           {/* End Icon Block */}
           {/* Icon Block */}
 
